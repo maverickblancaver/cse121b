6d3d7f9fd97678b55ad63017cacf6d006f9397df
maverickblancaver <blancaver63@gmail.com>
2024-02-21 11:23:59 +0800

    project

--- scripts/project_json.js
+++ scripts/project_json.js
@@ -1,15 +1,18 @@
-// script.js
+// JavaScript code for JSON 
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Fetch JSON data from the provided URL
-    fetch('https://run.mocky.io/v3/199b3ad6-ef71-4b9e-a83c-edb628deac7b')
-        .then(response => response.json())
-        .then(data => {
-            // Create HTML representation of the JSON data
-            const jsonDataDiv = document.getElementById('jsonData');
-            jsonDataDiv.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
-        })
-        .catch(error => {
-            console.error('Error fetching JSON:', error);
-        });
-});
+    // Fetch and display JSON data
+    fetch('https://byui-cse.github.io/cse121b-ww-course/resources/temples.json')
+      .then(response => response.json())
+      .then(data => {
+        const userDataElement = document.getElementById('userData');
+        userDataElement.innerHTML = `
+          <p>Name: ${data.name}</p>
+          <p>Age: ${data.age}</p>
+          <p>City: ${data.city}</p>
+          <p>Interests: ${data.interests.join(', ')}</p>
+        `;
+      })
+      .catch(error => console.error('Error fetching JSON:', error));
+  });
+  
--- scripts/project_tl_arrays.js
+++ scripts/project_tl_arrays.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const fruits = ['Apple', 'Banana', 'Orange', 'Grapes', 'Watermelon'];
 
-  // Call the displayArray function when the DOM is loaded
   displayArray();
 
   function displayArray() {
